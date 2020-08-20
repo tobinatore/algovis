f4c7312196574564fc733c9d09b4b0dea26b08fa
Tobias Nöthlich <tobinatore@users.noreply.github.com>
2020-08-20 11:55:02 +0200

    Started working on weighted graphs
    - reduced width of edges in visualization

--- js/graph.js
+++ js/graph.js
@@ -44,3 +44,48 @@ class UnweightedDirectedGraph{
         return this.adjList.length;
     }
 }
+
+class WeightedUndirectedGraph{
+    constructor(){
+        this.adjList = new Map();
+    }
+
+    addVertex(v){
+        this.adjList.set(v, []);
+    }
+
+    addEdge(v, w){ 
+        
+        if(!this.edgeExists(v,w)){
+            var length = Math.round(Math.sqrt(Math.pow((v.x - w.x),2)+Math.pow((v.y - w.y),2)));
+            this.adjList.get(v.id).push({id:w.id, len:length});
+            this.adjList.get(w.id).push({id:v.id, len:length});
+        }
+    }
+
+    edgeExists(v,w){
+        var array = this.adjList.get(v);
+        if(!array) return false;
+            
+        for (let i=0; i<array.length; i++) {
+            if (array[i].id === w) {
+                return true;
+            }
+        }
+            
+        return false;        
+    }
+
+    getNoOfVertices(){
+        return this.adjList.length;
+    }
+
+    getEdgeLength(v1,v2){
+        var array = this.adjList.get(v1);        
+        for (let i=0; i<array.length; i++) {
+            if (array[i].id === v2) {
+              return array[i].len;
+            }
+          }
+    }
+};
